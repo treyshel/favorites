a03f7d11f0a2fa14418eb0f71799e7548c04c696
Trey Shelton <tshelton@basecampcodingacademy.org>
2017-07-12 09:16:17 -0500

    import time to use in checkin time function

---
+++ student-checkin/checkin_core.py
@@ -0,0 +1,12 @@
+# Students should be able to check in
+# Instructors should be able to review:
+# All check in times
+# Check in times for a particular day
+# Check in times for a particular student 
+
+import time 
+
+
+def checkin_name(name):
+    ''' str -> str '''
+    for n in names:
